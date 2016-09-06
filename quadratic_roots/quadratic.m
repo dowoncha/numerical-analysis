1ead235846ecd16c0ac51991f01ba9ded1173f29
Dowon Cha <dowoncha@live.unc.edu>
2016-09-06 16:17:35 -0400

    Update and rename newtons.m to quadratic.m

--- quadratic_roots/quadratic.m
+++ quadratic_roots/quadratic.m
@@ -2,12 +2,13 @@ function [x0, x1] = newtons(a, b, c)
     % Finds root of a quadratic using quadratic equation
     % Considering two forms of the quadratic formula
     % Inaccuracies occur due to +- of same sign values and floating point numbers
+    % Worried about b >> ac so we check value of b to make sure inside of sqrt is okay
     
-    % @param f A function, easiest to pass an anonymous function
-    %   e.g. newtons(@(x)x^2, ...)
-    % @param x0 The inital start value to begin root search
-    % @param n # of iterations
-    % @return x Calculated x root value    
+    % @param a x^2 coefficient
+    % @param b x coefficient
+    % @param c constant
+    % @return x0, x1 roots
+    
     if (b >= 0) 
         x0 = (-b - sqrt(b* b-4*a*c)) / 2*a;
         x1 = 2*c/(-b-sqrt(b*b-4*a*c));
