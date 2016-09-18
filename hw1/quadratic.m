26d967d04c9b7bde75bef6cfafe95565cf950992
Dowon Cha <dowoncha@live.unc.edu>
2016-09-17 20:25:45 -0400

    merged

--- hw1/quadratic.m
+++ hw1/quadratic.m
@@ -1,15 +1,15 @@
-function [x0, x1] = quadratic(a, b, c)
+function [x0, x1] = newtons(a, b, c)
     % Finds root of a quadratic using quadratic equation
     % Considering two forms of the quadratic formula
     % Inaccuracies occur due to +- of same sign values and floating point numbers
     % Worried about b >> ac so we check value of b to make sure inside of sqrt is okay
-
+    
     % @param a x^2 coefficient
     % @param b x coefficient
     % @param c constant
     % @return x0, x1 roots
-
-    if (b >= 0)
+    
+    if (b >= 0) 
         x0 = (-b - sqrt(b* b-4*a*c)) / 2*a;
         x1 = 2*c/(-b-sqrt(b*b-4*a*c));
     else
